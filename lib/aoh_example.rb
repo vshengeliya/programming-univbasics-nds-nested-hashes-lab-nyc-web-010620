f316e8ed86e9a554b73f4a2db45d8710c945eb75
bright-format-7059 <v.shengeliya@gmail.com>
2019-11-18 03:51:38 +0000

    Done.

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -6,20 +6,43 @@ PAT_M =  { name:  "Pat Monteseian", occupation: "Staff" }
 KATE_G = { name:  "Kate Gompert", occupation: "None" }
 BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
 
-def assembled_aoh
+def assembled_aoh = [
+  {name:  "Don Gately", occupation: "Live-in Staff" },
+  { name:  "Joelle van Dyne", occupation: "Radio Personality" },
+  { name:  "Pat Monteseian", occupation: "Staff" },
+  { name:  "Kate Gompert", occupation: "None" },
+  { name:  "Bruce Green", occupation: "Fan of Mildred" },]
+ 
   # Build an array that contains (or, "nests") the constants into a single
   # Array. Ruby constants are denoted by ALL_CAPS
 end
 
-def literal_aoh
+def literal_aoh=
+  [
+  {name:  "Don Gately", occupation: "Live-in Staff" },
+  { name:  "Joelle van Dyne", occupation: "Radio Personality" },
+  { name:  "Pat Monteseian", occupation: "Staff" },
+  { name:  "Kate Gompert", occupation: "None" },
+  { name:  "Bruce Green", occupation: "Fan of Mildred" },]
+  
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
 end
 
 def aoh_lookup(aoh, row, key)
+  puts aoh_lookup[2][:name]
 end
 
-def aoh_update(aoh, row, key, new_value)
+def aoh_update(aoh, row, key, new_value) =
+  [
+  {name:  "Don Gately", occupation: "Live-in Staff" },
+  { name:  "Joelle van Dyne", occupation: "Radio Personality" },
+  { name:  "Pat Monteseian", occupation: "Staff" },
+  { name:  "Kate Gompert", occupation: "None" },
+  { name:  "Bruce Green", occupation: "Fan of Mildred" },]
+  
+  aoh_update[1][:name]= "Joelle van Dyne (PGOAT)"
+  p aoh_update[1][:name]
   # Update the AoH data at row and key to have the value of new_value
   # Return the updated AoH
 end
