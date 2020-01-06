a97426f0ad60e8096f22a9df4edad8e137068685
Veronika Dodda <v.shengeliya@gmail.com>
2020-01-06 02:21:38 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -1,3 +1,4 @@
+Learn more or give us feedback
 # Examples inspired by David Foster Wallace's # (2/21/1962 to 9/12/2008) _Infinite Jest_
 
 DON_G = { name:  "Don Gately", occupation: "Live-in Staff" }
@@ -6,43 +7,29 @@ PAT_M =  { name:  "Pat Monteseian", occupation: "Staff" }
 KATE_G = { name:  "Kate Gompert", occupation: "None" }
 BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
 
-def assembled_aoh = [
-  {name:  "Don Gately", occupation: "Live-in Staff" },
-  { name:  "Joelle van Dyne", occupation: "Radio Personality" },
-  { name:  "Pat Monteseian", occupation: "Staff" },
-  { name:  "Kate Gompert", occupation: "None" },
-  { name:  "Bruce Green", occupation: "Fan of Mildred" },]
- 
+def assembled_aoh
   # Build an array that contains (or, "nests") the constants into a single
   # Array. Ruby constants are denoted by ALL_CAPS
+  [DON_G, JOELLE_VD, PAT_M, KATE_G, BRUCE_G]
 end
 
-def literal_aoh=
-  [
-  {name:  "Don Gately", occupation: "Live-in Staff" },
-  { name:  "Joelle van Dyne", occupation: "Radio Personality" },
-  { name:  "Pat Monteseian", occupation: "Staff" },
-  { name:  "Kate Gompert", occupation: "None" },
-  { name:  "Bruce Green", occupation: "Fan of Mildred" },]
-  
+def literal_aoh
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
+  [{:name=>"Don Gately", :occupation=>"Live-in Staff"},
+       {:name=>"Joelle van Dyne", :occupation=>"Radio Personality"},
+       {:name=>"Pat Monteseian", :occupation=>"Staff"},
+       {:name=>"Kate Gompert", :occupation=>"None"},
+       {:name=>"Bruce Green", :occupation=>"Fan of Mildred"}]
 end
 
 def aoh_lookup(aoh, row, key)
-  puts aoh_lookup[2][:name]
+  aoh[row][key]
 end
 
-def aoh_update(aoh, row, key, new_value) =
-  [
-  {name:  "Don Gately", occupation: "Live-in Staff" },
-  { name:  "Joelle van Dyne", occupation: "Radio Personality" },
-  { name:  "Pat Monteseian", occupation: "Staff" },
-  { name:  "Kate Gompert", occupation: "None" },
-  { name:  "Bruce Green", occupation: "Fan of Mildred" },]
-  
-  aoh_update[1][:name]= "Joelle van Dyne (PGOAT)"
-  p aoh_update[1][:name]
+def aoh_update(aoh, row, key, new_value)
   # Update the AoH data at row and key to have the value of new_value
   # Return the updated AoH
+  aoh[row][key] = new_value
+  aoh
 end
